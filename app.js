57716231d830642107613322146b8069bb068746
Mark Reid <contact@markreid.com.au>
2014-02-11 21:12:17 +1100

    there's your express app, buddy.

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.configure(function(){
+    io.enable('log');
+    io.set('log level', 2);
+});
+
+io.sockets.on('connection', function(socket){
+    console.info('socket connected');
+
+    socket.on('play', function(sampleId){
+        console.log(sampleId);
+        io.sockets.emit('play', sampleId);
+    });
+
+
+
+});
