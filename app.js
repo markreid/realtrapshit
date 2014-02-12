48d4cbec3c46cb316d9c8ba1d4dcfe530fbe171d
Mark Reid <contact@markreid.com.au>
2014-02-12 13:47:03 +1100

    dodgy google auth stuff

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var cookie = require('cookie');
 var passport = require('passport');
 var passportGoogleStrategy = require('passport-google').Strategy;
+// todo - this looks dodgy
+var MemoryStore = require('./node_modules/express/node_modules/connect/lib/middleware/session/memory');
 
 // settings file
 var config = require('./config');
@@ -18,6 +21,7 @@ var config = require('./config');
 
 // instantiate and configure expressjs
 var app = express();
+var sessionStore = new MemoryStore();
 
 app.set('port', config.port || 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({secret:'secrety secrets'}));
+app.use(express.session({secret:'secrety secrets', store:sessionStore}));
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -49,7 +53,7 @@ app.get('/auth/google', passport.authenticate('google', {failureRedirect: '/logi
 	res.redirect('/');
 });
 app.get('/auth/google/return', passport.authenticate('google', {failureRedirect: '/login'}), function(req, res){
-	console.info('success, i think');
+	console.info('successfully authenticated via google');
 	res.redirect('/');
 });
 
@@ -57,13 +61,9 @@ app.get('/auth/google/return', passport.authenticate('google', {failureRedirect:
 
 // configure passport auth
 passport.serializeUser(function(user, done){
-	// console.log('serialize');
-	// console.log(user);
 	done(null, user);
 });
 passport.deserializeUser(function(obj, done){
-	// console.log('deserialize');
-	// console.log(obj);
 	done(null, obj);
 });
 
@@ -86,17 +86,41 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 });
 
 
-
 // set up the sockets
 var io = require('socket.io').listen(server);
 io.configure(function(){
     io.enable('log');
     io.set('log level', 2);
+    io.set('authorization', function socketHandshake(handshakeData, accept){
+    	if(!handshakeData.headers.cookie) return accept('No cookies in handshakeData');
+
+		var handshakeCookie = cookie.parse(handshakeData.headers.cookie);
+		var sessionId = handshakeCookie['connect.sid'];
+		if(!sessionId) return accept('No Express SID in cookie');
+
+		sessionStore.get(sessionId.substr(2,24), function(err, session){
+			if(err || !session) return accept('No matching session in store.');
+
+			// attach the session to handshakedata so we can grab it later
+			handshakeData.session = session;
+			accept(null, true);
+		});
+    });
 });
 
 // socket listeners
 io.sockets.on('connection', function(socket){
-    console.info('socket connected');
+	var user;
+	try{
+		user = socket.handshake.session.passport.user;
+	} catch(e){
+		console.error('No user in socket session');
+		socket.disconnect();
+	}
+
+	console.info(user.name.givenName + ' connected to socket');
+
+    socket.broadcast.emit('user.connected', user.name.givenName);
 
     socket.on('play', function(sampleId){
         console.log(sampleId);
